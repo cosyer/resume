7e7bfdafe0a60d8da763655e9e58d1a54d251588
chenyu <535509852@qq.com>
2019-04-10 23:21:39 +0800

    begin

---
+++ README.md
@@ -0,0 +1,3 @@
+请多指教O(∩_∩)O哈哈~ 
+
+https://cosyer.github.io/resume/
---
+++ js/sp.js
@@ -0,0 +1,10 @@
+$(function() {
+  // 移动端连接跳转
+  $("#Ilinks li").on("click", function() {
+    window.open(
+      $(this)
+        .find("a")
+        .attr("href")
+    );
+  });
+});
